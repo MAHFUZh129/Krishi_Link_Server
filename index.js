46b17af846a7ff43b28cb073a0b03656f2357185
mahfuzh129 <mahfuzh266@gmail.com>
2025-11-10 17:44:16 +0600

    added user name and  password in uri to be connected to mongodb

--- index.js
+++ index.js
@@ -5,9 +5,40 @@ const port = 3000
 app.use(cors())
 app.use(express.json())
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://krishi-link:TxVY3DfzUZ5EjOYX@cluster0.7gwzlnt.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`)
